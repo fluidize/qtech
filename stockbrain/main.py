16e63ca82b48fd67c4ced4260066a884bce1fdc2
fluidize <mbxiao1@gmail.com>
2024-12-10 16:51:46 -0600

    alpaca

--- stockbrain/main.py
+++
@@ -1,7 +0,0 @@
-from alpaca.trading.client import TradingClient
-API_KEY_ID = "PKACVJJ8HOHIPE5EFP0Z"
-API_SECRET_KEY = "gRXq5qom5Oxjb0dvMBwPmJLNlUrUhTtE3JMKeiHn"
-
-trading_client = TradingClient(API_KEY_ID, API_SECRET_KEY, paper=True)
-account = trading_client.get_account()
-print(account)
