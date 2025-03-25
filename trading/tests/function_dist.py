ea22499dfc87b1f49aca8ca83fd7bd3c000ffcdb
Fluidize <mbxiao1@gmail.com>
2025-03-25 16:27:04 -0500

    updated some st environment functions, trying out gridsearching to optimize algorithms

--- trading/tests/function_dist.py
+++ trading/tests/function_dist.py
@@ -45,30 +45,34 @@ def _calculate_macd(context, fast_period=12, slow_period=26, signal_period=9):
 
     return macd_line, signal_line, histogram
 
+def chi_distribution(data):
+    chi_df, chi_loc, chi_scale = stats.chi.fit(data)
+
+    linspace_mean = np.mean(data)
+    linspace_std = np.std(data)
+    x = np.linspace(np.min(data) - linspace_std, np.max(data) + linspace_std, 1000)
+    pdf = stats.chi2.pdf(x, chi_df, loc=chi_loc, scale=chi_scale)
+    cdf = stats.chi2.cdf(x, chi_df, loc=chi_loc, scale=chi_scale)
+
+    
+    # derivative_cdf = np.gradient(cdf, x)
+    print(np.sum(data < 0))
+
+    fig = sp.make_subplots(rows=2, cols=1, 
+                        subplot_titles=('PDF', 'CDF'))
+    fig.add_trace(go.Scatter(x=x, y=pdf, mode='lines', name='PDF'), row=1, col=1)
+    # fig.add_vline(x=linspace_std, line=dict(color='red', dash='dash'), row=1, col=1)
+    # fig.add_trace(go.Histogram(x=std, name='Histogram'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=x, y=cdf, mode='lines', name='CDF'), row=2, col=1)
+    # fig.add_trace(go.Scatter(x=x, y=derivative_cdf, mode='lines', name="CDF'"), row=1, col=1)
+
+    fig.show()
+
 data = fetch_data('BTC-USD', 29, '5m', 10)
 
 # Calculate returns instead of standard deviation
 raw_std = data['Close'].rolling(window=20).std()
 raw_std.dropna(inplace=True, axis=0)
+data_std = (raw_std - raw_std.min())/(raw_std.max() - raw_std.min())
 
-std = (raw_std - raw_std.min())/(raw_std.max() - raw_std.min())
-
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(std)
-
-chi_df, chi_loc, chi_scale = stats.chi.fit(std)
-x = np.linspace(np.mean(std) - 4*np.std(std), np.mean(std) + 4*np.std(std), 1000)
-pdf = stats.chi2.pdf(x, chi_df, loc=chi_loc, scale=chi_scale)
-cdf = stats.chi2.cdf(x, chi_df, loc=chi_loc, scale=chi_scale)
-# derivative_cdf = np.gradient(cdf, x)
-print(np.sum(std < 0))
-
-fig = sp.make_subplots(rows=2, cols=1, 
-                      subplot_titles=('PDF', 'CDF'))
-fig.add_trace(go.Scatter(x=x, y=pdf, mode='lines', name='PDF'), row=1, col=1)
-# fig.add_trace(go.Histogram(x=std, name='Histogram'), row=1, col=1)
-fig.add_trace(go.Scatter(x=x, y=cdf, mode='lines', name='CDF'), row=2, col=1)
-# fig.add_trace(go.Scatter(x=x, y=derivative_cdf, mode='lines', name="CDF'"), row=1, col=1)
-
-fig.show()
-
+chi_distribution(data_std)
