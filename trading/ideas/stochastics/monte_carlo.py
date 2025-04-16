9fff21bb9e2a76404ca1617cd80a707f71d7de5e
fluidize <mbxiao1@gmail.com>
2025-04-15 19:45:07 -0500

    added option for wiener process to be geometric. added distribution histogram in montecarlo sims

--- trading/ideas/stochastics/monte_carlo.py
+++ trading/ideas/stochastics/monte_carlo.py
@@ -2,28 +2,37 @@ import wiener_process as wiener
 import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from plotly.subplots import make_subplots
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 def monte_carlo_simulation(n_steps, n_paths, time_unit=1):
+    # if process is log-normal, taking a logarithm reverses the exponential transformation, turning it into a normal distribution
     paths = []
     
     variance = time_unit / n_steps
     stddev = np.sqrt(variance)
 
-    for i in range(n_paths):
-        paths.append(wiener.wiener_process(n_steps, time_unit=time_unit, live_plot=False))
+    for i in tqdm(range(n_paths), desc="Simulating paths"):
+        paths.append(wiener.wiener_process(n_steps, time_unit=time_unit, gbm=True, live_plot=False))
     
     paths_array = np.array(paths)
     
     df = pd.DataFrame(paths_array.T) 
 
     mean_path = df.mean(axis=1)
-    stddev_path = df.std(axis=1)
+    #66% - 95% - 99.7% rule
+    std_mult = 2
+    stddev_path = df.std(axis=1) * std_mult
 
-    fig = go.Figure()
+    fig = make_subplots(rows=2, cols=1, subplot_titles=("Simulated Paths", "Distributions"))
+
     for i in range(df.shape[1]):
-        fig.add_trace(go.Scatter(x=df.index, y=df[i], mode='lines', name=f'Path {i+1}'))
+        fig.add_trace(go.Scatter(x=df.index, y=df[i], mode='lines', name=f'Path {i+1}'), row=1, col=1)
 
-    # Add shaded area for standard deviation
+    fig.add_trace(go.Scatter(x=df.index, y=mean_path, mode='lines', name='Mean Path', line=dict(color='blue', width=2)), row=1, col=1)
+
     fig.add_trace(go.Scatter(
         x=df.index,
         y=mean_path + stddev_path,
@@ -31,25 +40,36 @@ def monte_carlo_simulation(n_steps, n_paths, time_unit=1):
         line=dict(color='lightblue', dash='dash'),
         name='Mean + Std Dev',
         showlegend=False
-    ))
+    ), row=1, col=1)
     fig.add_trace(go.Scatter(
         x=df.index,
         y=mean_path - stddev_path,
         mode='lines',
         line=dict(color='lightblue', dash='dash'),
-        fill='tonexty',  # Fill area between the two lines
+        fill='tonexty',
         name='Variance Area',
         showlegend=False
-    ))
+    ), row=1, col=1)
 
-    # Update layout
+    final_values = df.iloc[-1]
+    fig.add_trace(go.Histogram(x=final_values, name='Final Values', opacity=0.75, nbinsx=1000), row=2, col=1)
+
     fig.update_layout(
-        title='Shaded Area Representing Variance Around Mean Path',
+        title=f'Monte Carlo Simulation | {n_paths} paths | {n_steps} steps | {stddev:.2f} Standard Deviation',
         xaxis_title='Steps',
-        yaxis_title='Wiener Process Value'
+        yaxis_title='Process Value',
+        template='plotly_dark'
     )
 
     fig.show()
 
+    plt.figure(figsize=(8, 6))
+    stats.probplot(np.log(final_values), dist="norm", plot=plt)
+    plt.title('Q-Q Plot of Log-Transformed Final Values')
+    plt.xlabel('Theoretical Quantiles')
+    plt.ylabel('Sample Quantiles')
+    plt.grid()
+    plt.show()
+
 if __name__ == "__main__":
-    paths = monte_carlo_simulation(1000, 100, time_unit=1)
+    paths = monte_carlo_simulation(n_steps=1000, n_paths=5000, time_unit=1)
--- trading/ideas/stochastics/wiener_process.py
+++ trading/ideas/stochastics/wiener_process.py
@@ -1,18 +1,20 @@
 import numpy as np
 import plotly.graph_objects as go
 
-def wiener_process(n_steps, time_unit=1, live_plot=False):
+def wiener_process(n_steps, time_unit=1, gbm=True, live_plot=False):
+    #set w to 1 and multiply by delta_w to get log normal distribution
     y = []
-    w = 0
 
-    frames = []  # List to hold frames
+    w = 1 if gbm else 0
+
+    frames = []
 
     for i in range(n_steps):
         delta_t = time_unit / n_steps
         # in a wiener process, the variance of the increment delta_w is delta_t. in order to get the stddev, we sqrt
         # the variance of each step is: Var[ΔW]=Δt
-        delta_w = np.random.normal(0, np.sqrt(delta_t)) #numpy requires stddev, not variance
-        w += delta_w
+        delta_w = np.random.normal(1 if gbm else 0, np.sqrt(delta_t)) #numpy requires stddev, not variance
+        w = w * delta_w if gbm else w + delta_w
         y.append(w)
 
         if live_plot:
@@ -55,4 +57,4 @@ def wiener_process(n_steps, time_unit=1, live_plot=False):
     return y
 
 if __name__ == "__main__":
-    w = wiener_process(1000, time_unit=10, live_plot=True)
+    w = wiener_process(1000, time_unit=10, gbm=False, live_plot=True)
