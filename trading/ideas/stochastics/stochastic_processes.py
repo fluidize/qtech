f8b28e1b22fa1a598254a874de285bb20439e9bd
fluidize <mbxiao1@gmail.com>
2025-04-16 21:24:58 -0500

    combine files

--- trading/ideas/stochastics/stochastic_processes.py
+++ trading/ideas/stochastics/stochastic_processes.py
@@ -1,4 +1,3 @@
-import wiener_process as wiener
 import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
@@ -6,6 +5,20 @@ from tqdm import tqdm
 from plotly.subplots import make_subplots
 import scipy.stats as stats
 import matplotlib.pyplot as plt
+
+def wiener_process(n_steps, time_unit=1, gbm=True):
+    #set w to 1 and multiply by delta_w to get log normal distribution
+    y = []
+    w = 1 if gbm else 0
+
+    for i in range(n_steps * time_unit):
+        delta_t = time_unit / n_steps
+        # in a wiener process, the variance of the increment delta_w is delta_t. in order to get the stddev, we sqrt
+        # the variance of each step is: Var[ΔW]=Δt
+        delta_w = np.random.normal(1 if gbm else 0, np.sqrt(delta_t)) #numpy requires stddev, not variance
+        w = (w * delta_w) if gbm else (w + delta_w)
+        y.append(w)
+    return y
 
 def monte_carlo_simulation(n_steps, n_paths, time_unit=1):
     # if process is log-normal, taking a logarithm reverses the exponential transformation, turning it into a normal distribution
@@ -15,7 +28,7 @@ def monte_carlo_simulation(n_steps, n_paths, time_unit=1):
     stddev = np.sqrt(variance)
 
     for i in tqdm(range(n_paths), desc="Simulating paths"):
-        paths.append(wiener.wiener_process(n_steps, time_unit=time_unit, gbm=True))
+        paths.append(wiener_process(n_steps, time_unit=time_unit, gbm=True))
     
     paths_array = np.array(paths)
     
@@ -55,5 +68,4 @@ def plot_paths(df):
 
 if __name__ == "__main__":
     paths = monte_carlo_simulation(n_steps=1000, n_paths=1000, time_unit=1)
-    fig = go.Figure(data=go.Histogram(x=paths, nbinsx=1000))
-    fig.show()
+    plot_paths(paths)
