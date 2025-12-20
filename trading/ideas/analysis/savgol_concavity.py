4f11a192f244e17c1f212e1e18ba51e3af94ea9f
fluidize <mbxiao1@gmail.com>
2025-12-19 22:24:16 -0600

    savitzky-golay price smoothing

---
+++ trading/ideas/analysis/savgol_concavity.py
@@ -0,0 +1,21 @@
+import scipy
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append("")
+
+import trading.model_tools as mt
+
+#use the savitzky golay filter to estimate price acceleration
+
+data = mt.fetch_data("BTC-USDT", days=1, interval="5m", age_days=0, data_source="binance")
+
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(10, 10), sharex=True)
+
+window = 7
+
+ax1.plot(data["Close"])
+ax1.plot(data["Close"].rolling(window=window).mean(), color="red")
+ax2.plot(scipy.signal.savgol_filter(data["Close"].rolling(window=window).mean(), window_length=10, polyorder=4, deriv=1))
+
+plt.show()
