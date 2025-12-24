3d6f96a705441915e7504b7430ff4a7d1d6fd7dd
fluidize <mbxiao1@gmail.com>
2025-12-23 22:37:52 -0600

    paramspecs to optim search space dict

--- trading/brains/evolution/gp_tools.py
+++ trading/brains/evolution/gp_tools.py
@@ -1,19 +1,26 @@
 import ast
-from param_space import registered_param_specs
+from param_space import registered_param_specs, ParamSpec
 import random
 
 import sys
 sys.path.append("")
 import trading.technical_analysis as ta
 
-def get_available_indicators():
-    """Get all indicator functions from ta module."""
+def get_indicators(exclude: list[str] = ["hma", "percent_rank"]):
+    """ Get indicator functions from ta module """
+    exclude_set = set(exclude)
     indicators = []
     for name in registered_param_specs.keys():
-        if hasattr(ta, name):
+        if name not in exclude_set and hasattr(ta, name):
             indicators.append(getattr(ta, name))
     return indicators
 
 def random_operator():
     """Get random comparison operator."""
     return random.choice([ast.Gt(), ast.Lt()])
+
+def paramspecs_to_dict(param_specs: list[ParamSpec]) -> dict:
+    return {p.parameter_name : p.search_space for p in param_specs}
+
+def heuristic_fitness(strategy: callable) -> float:
+    pass
