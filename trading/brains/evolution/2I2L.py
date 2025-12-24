bf186f273250d61f537dff64bf1bfd3d0033d17e
fluidize <mbxiao1@gmail.com>
2025-12-23 22:37:30 -0600

    random 2i2l algorithm example

--- trading/brains/evolution/2I2L.py
+++ trading/brains/evolution/2I2L.py
@@ -1,14 +1,17 @@
 #two indicators two logic genes
 import random
+import ast
 
 from ast_builder import Builder
 from ast_tools import unparsify
 from genes import IndicatorGene, IndicatorToConstant, IndicatorToPrice, IndicatorToIndicator
-from gp_tools import get_available_indicators, random_operator
+from gp_tools import get_indicators, random_operator, paramspecs_to_dict
+
+from rich import print
 
 def generate_individual(num_indicators=2, num_logic=2):
     """Generate one random individual."""
-    available = get_available_indicators()
+    available = get_indicators()
     
     # Random indicators
     indicator_genes = [
@@ -42,9 +45,43 @@ def generate_population(size=100, num_indicators=2, num_logic=2):
 
 # Example usage
 if __name__ == "__main__":
+    import sys
+    sys.path.append("")
+    import trading.technical_analysis as ta
+    from trading.backtesting.algorithm_optim import QuantitativeScreener
+    import pandas as pd
+    
+    
+    engine = QuantitativeScreener(
+        symbols=["SOL-USDT"],
+        days=365,
+        intervals=["1d"],
+        age_days=0,
+        data_source="binance",
+        initial_capital=10000,
+        slippage_pct=0.005,
+        commission_fixed=0.00,
+        cache_expiry_hours=24
+    )
     # Single individual
     builder = generate_individual(num_indicators=2, num_logic=2)
     base_ast, algorithm_parameter_specs = builder._construct_algorithm_base()
     unparsify(base_ast)
     
-    population = generate_population(size=50, num_indicators=2, num_logic=2)
+    base_func = compile(ast.fix_missing_locations(ast.Module(body=[base_ast])), "<string>", "exec")
+    namespace = {"ta": ta, "pd": pd}
+    exec(base_func, namespace)
+
+    base_func = namespace["strategy"]
+    base_param_space = paramspecs_to_dict(algorithm_parameter_specs)
+
+    engine.optimize(
+        strategy_func=base_func,
+        param_space=base_param_space,
+        metric="Sharpe_Ratio",
+        n_trials=100,
+        direction="maximize",
+        save_params=False
+    )
+
+    engine.plot_best_performance(mode="standard")
