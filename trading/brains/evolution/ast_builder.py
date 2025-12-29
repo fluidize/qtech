355ce82dc4a23cbd82a8f4c0ad3f778a8535839c
fluidize <mbxiao1@gmail.com>
2025-12-28 22:28:01 -0600

    add variable name persistence on LogicGenes to prevent breaking on recalls

--- trading/brains/evolution/ast_builder.py
+++ trading/brains/evolution/ast_builder.py
@@ -114,7 +114,7 @@ class IndicatorToIndicator(LogicGene):
     def load_indicators(self, indicator_variable_names: list[str]):
         self.left_indicator_variable_name = indicator_variable_names[self.left_index]
         self.right_indicator_variable_name = indicator_variable_names[self.right_index]
-        self.variable_name = f"LOGIC_{self.left_indicator_variable_name}_{self.right_indicator_variable_name}"
+        self.variable_name = f"LOGIC_{self.left_indicator_variable_name}_{self.right_indicator_variable_name}" if self.variable_name is None else self.variable_name
     
     def get_name(self):
         return self.variable_name
@@ -144,7 +144,7 @@ class IndicatorToPrice(LogicGene):
     
     def load_indicators(self, indicator_variable_names: list[str]):
         self.left_indicator_variable_name = indicator_variable_names[self.left_index]
-        self.variable_name = f"LOGIC_{self.left_indicator_variable_name}_{self.columns[self.column_index]}"
+        self.variable_name = f"LOGIC_{self.left_indicator_variable_name}_{self.columns[self.column_index]}" if self.variable_name is None else self.variable_name
     
     def get_name(self):
         return self.variable_name
@@ -178,7 +178,7 @@ class IndicatorToConstant(LogicGene):
     
     def load_indicators(self, indicator_variable_names: list[str]):
         self.left_indicator_variable_name = indicator_variable_names[self.left_index]
-        self.variable_name = f"LOGIC_{self.left_indicator_variable_name}_const_{unique_counter()}"
+        self.variable_name = f"LOGIC_{self.left_indicator_variable_name}_const_{unique_counter()}" if self.variable_name is None else self.variable_name
         self.parameter_specs = [ParamSpec(parameter_name=f"{self.variable_name}_constant", search_space=(-100, 100))]
     
     def get_name(self):
