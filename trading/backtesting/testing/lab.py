c8c82588534953da9b1a8e96c92f3dde531383af
fluidize <mbxiao1@gmail.com>
2026-01-02 16:23:59 -0600

    testing continous space

--- trading/backtesting/testing/lab.py
+++ trading/backtesting/testing/lab.py
@@ -1,5 +1,6 @@
 from rich import print
 import pandas as pd
+import numpy as np
 
 import sys
 sys.path.append("")
@@ -9,36 +10,35 @@ import trading.backtesting.testing.cstrats as cs
 import trading.backtesting.basic_strategies as bs
 import trading.technical_analysis as ta
 
-def strategy(data, fast_ma_window, slow_ma_window):
+def strategy(data, ma_window):
     signals = pd.Series(0, index=data.index)
-    fast_ma = ta.sma(data['Close'], timeperiod=fast_ma_window)
-    slow_ma = ta.sma(data['Close'], timeperiod=slow_ma_window)
-    signals[fast_ma > slow_ma] = 1
-    signals[fast_ma < slow_ma] = 0
-    return signals
+    ma = ta.ema(data['Close'], timeperiod=ma_window)
+    gap = ((data['Close'] - ma) / data['Close']) * 50
+    signals = -gap
+    return np.tanh(signals)
 
 #training
 qs = QuantitativeScreener(
-    symbols=["SOL-USDT"],
+    symbols=["JUP-USDT"],
     days=365,
-    intervals=["30m"],
+    intervals=["15m"],
     age_days=0,
     data_source="binance",
     initial_capital=10000,
-    slippage_pct=0.03,
+    slippage_pct=0.01,
     commission_fixed=0.00,
     cache_expiry_hours=48
 )
 
 qs.optimize(
     strategy_func=strategy,
-    param_space={"fast_ma_window": (2, 200), "slow_ma_window": (2, 200)},
-    metric="Alpha * np.clip(1 + Max_Drawdown, 0, None)",
+    param_space={"ma_window": (2, 200)},
+    metric="Alpha * np.clip((1 + Max_Drawdown), 0, None)",
     n_trials=100,
     direction="maximize",
     save_params=False
 )
-qs.plot_best_performance(mode="standard")
+qs.plot_best_performance(mode="tradingview")
 print(qs.get_best_metrics())
 best_set = qs.get_best()
 print(best_set)
