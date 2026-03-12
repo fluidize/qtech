2727cd1d3f401dbb6eb7575b253c1cdde0e835df
fluidize <mbxiao1@gmail.com>
2026-03-11 22:06:56 -0500

    update hmm

--- trading/ideas/stochastics/hmm.py
+++ trading/ideas/stochastics/hmm.py
@@ -1,58 +1,49 @@
 import numpy as np
-import plotly.graph_objects as go
-import plotly.io as pio
+import matplotlib.pyplot as plt
 from hmmlearn import hmm
+from scipy.signal import savgol_filter
 
 import sys
 sys.path.append("trading")
 import model_tools as mt
 
-pio.renderers.default = "browser"
-
-data = mt.fetch_data("BTC-USDT", chunks=730, interval="1d", age_days=0, 
+data = mt.fetch_data("BTC-USDT", days=100, interval="1h", age_days=0,
                     data_source="binance").drop("Datetime", axis=1)
 
-X = data[['Close']].pct_change().dropna()
+X = savgol_filter(data['Close'], window_length=40, polyorder=3, deriv=1)
+X = np.expand_dims(np.asarray(X), axis=1)
+price = data['Close'].values
 
 model = hmm.GaussianHMM(
-    n_components=2, 
-    covariance_type="diag", 
+    n_components=2,
+    covariance_type="diag",
     n_iter=10000,
     init_params="kmeans",
     random_state=42
 )
 model.fit(X)
 
-samples, states = model.sample(1000)
-time_index = np.arange(len(samples))
+states = model.predict(X)
+print(states)
+time_index = np.arange(len(states))
+baseline = np.full_like(price, price.min())
+cmap = plt.cm.viridis
+n_states = states.max() + 1
 
-fig = go.Figure()
-print(samples)
-fig.add_trace(go.Scatter(
-    x=time_index,
-    y=samples[:,0],
-    mode="lines",
-    name="Generated Prices",
-    line=dict(color='blue')
-))
+fig, ax = plt.subplots(figsize=(12, 5))
+ax.plot(time_index, price, color='black', linewidth=1)
 
-fig.add_trace(go.Scatter(
-    x=time_index,
-    y=states,
-    mode="markers",
-    name="Hidden States",
-    yaxis="y2",
-    marker=dict(color=states, colorscale="Viridis", size=5)
-))
+i = 0
+while i < len(states):
+    j = i
+    while j < len(states) and states[j] == states[i]:
+        j += 1
+    color = cmap(states[i] / n_states)
+    ax.fill_between(time_index[i:j], price[i:j], baseline[i:j], color=color, alpha=0.3)
+    i = j
 
-fig.update_layout(
-    title="HMM Simulation",
-    yaxis=dict(title="Price"),
-    yaxis2=dict(
-        title="State",
-        overlaying="y",
-        side="right",
-    )
-)
-
-fig.show()
+ax.set_xlabel('Time')
+ax.set_ylabel('Price')
+ax.set_title('Price by HMM State')
+fig.tight_layout()
+plt.show()
