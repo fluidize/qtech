72be4c33708cc6fa10da5c8ff66be9381dafcfd9
fluidize <mbxiao1@gmail.com>
2026-04-30 16:05:26 -0500

    create init installation file, move terminal outside /trading

--- init.sh
+++ init.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 source .venv/bin/activate
+
+install_dependencies() {
+    echo "Installing dependencies..."
+
+    pip install numpy pandas matplotlib seaborn plotly
+    
+    pip install scikit-learn scipy hmmlearn optuna optunahub lightgbm
+    
+    pip install yfinance requests aiohttp websockets
+    
+    pip install beautifulsoup4 selenium
+    
+    pip install rich tqdm textual
+    
+    pip install discord-webhook
+    
+    echo "All dependencies installed!"
+}
+
+install_dependencies
